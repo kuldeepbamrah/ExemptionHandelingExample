ab546b9dd0a6e887b65bf5faad3905fca62394a4
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-09-26 18:42:04 -0400

    Initial commit

---
+++ src/main/java/com/lambton/ExceptionHandling.java
@@ -0,0 +1,17 @@
+package com.lambton;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args)
+    {
+        int a= 100;
+        int b=0;
+        int c;
+        System.out.println("Start");
+
+        c=a/b;
+        System.out.println("C : " +c);
+        
+    }
+
+}
