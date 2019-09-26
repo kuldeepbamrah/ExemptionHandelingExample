1c9254a806d13e780e4d57c973fdbd75ac2265c9
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-09-26 18:53:23 -0400

    commit 3

--- src/main/java/com/lambton/ExceptionHandling.java
+++ src/main/java/com/lambton/ExceptionHandling.java
@@ -7,15 +7,27 @@ public class ExceptionHandling {
         int a= 100;
         int b=0;
         int c;
+
+        int[] x={1,2,3};
+
+
+
+
         System.out.println("Start");
 
         try {
-
+            x[4]=1000;
             c = a / b;
             System.out.println("C : " + c);
 
         } catch (ArithmeticException  ae) {
             System.out.println("Value of b = 0, Divisible by 0 error");
+        }
+        catch (ArrayIndexOutOfBoundsException  aob) {
+            System.out.println("Array x is is trying to access  index which is out of bound");
+        }
+        catch (Exception ae) {
+            System.out.println(ae.getMessage());
         }
 
         System.out.println("End");
